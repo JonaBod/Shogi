7fd4a6d81b8df80ea5a5949a4abeac1a351aaf7f
jonabodirsky <bodirsky24@up.edu>
2024-08-29 15:26:34 -0700

    Birthday first commit

---
+++ CS371/BirthdayCake-master/app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -0,0 +1,16 @@
+package cs301.birthdaycake;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_main);
+    }
+}
